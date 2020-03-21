4536876a119acecd764f8b61d0bd248eafeda9fc
Cesar GP <cgpcsjcmp@gmail.com>
2020-03-21 18:50:10 +0100

    feat(logger): added simple chalk logger

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const logger = require("./src/utils/logger");
+
 const addTwoNumbers = (a, b) => {
+  logger.info("Adding " + a + " and " + b);
   return a + b;
 };
 const addThreeNumbers = (a, b, c) => {
